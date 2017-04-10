30c7e582cde9868efac57b84fcfc2264365e44bc
sophiewxh <huiwang8921@gmail.com>
2017-04-10 17:57:22 -0400

    save plots

--- hurricane/Lili/bnlearn_test.R
+++ hurricane/Lili/bnlearn_test.R
@@ -33,15 +33,6 @@ plot(res)
 res <- gs(learning.test, blacklist = tiers2blacklist(list(c("A", "C", "F"), c("B", "D", "E"))))
 plot(res)
 
-#######################################################################################
-library(bnlearn)
-library(Rgraphviz)
-#data <- read.csv("C:/Users/Sophie/workspace/EvacModel/sample7.csv", header=TRUE)
-data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels.csv", header=TRUE)
-#data <- d1[,sample(ncol(d1))]
-#cor(data)
-
-
 # rowMax <- function(df) 
 # {
 #   apply(df, 1, max)
@@ -51,6 +42,21 @@ data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels.csv", heade
 # peer <- data[,20]
 # prev <- rowMeans(data[,c(23:24)])
 # finance <- rowMeans(data[,c(25:28)])
+
+#######################################################################################
+library(bnlearn)
+library(Rgraphviz)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels.csv", header=TRUE)
+#data <- d1[,sample(ncol(d1))]
+#cor(data)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_NotEvac.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_EarlyEvac.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_LateEvac.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_LowIncome.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_HighIncome.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_VeryLowIncome.csv", header=TRUE)
+#data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_Young.csv", header=TRUE)
+data <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels_Old.csv", header=TRUE)
 
 # 1-4
 demo <- c("Age", "Gender", "Race", "Marriage")
@@ -62,6 +68,7 @@ house <- c("Owner", "HouseStruct", "CloseCoast","CloseWater")
 official <- c("OfficialHurricWatch", "OfficialEvac")
 # 15-19
 info <- c("SrcLocalAuth", "SrcLocalMedia", "SrcNationalMedia", "SrcInternet", "SrcPeers")
+#local_info <- c("SrcLocalAuth", "SrcLocalMedia", "SrcPeers")
 # 20-22
 see <- c("SeeStormCond", "SeeShopClose", "SeePeerEvac")
 # 23-24
@@ -71,19 +78,26 @@ concern <- c("ProtectFromLooter", "ProtectFromStorm", "LostIncome", "EvacExpense
 # 30
 evac <- c("Evac")
 
-X <- data[, c(c(9:14), 20, 21, 22, 28, 30)]
+X <- data
+#X <- data[, c( 20, 21, 22, 28, 30)]
+
+#X <- data[, c(9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 28, 30)]
+#X <- data[, c( 11, 13, 14, 15, 19, 22, 30)]
   
 #X <- cbind(close, see, peer, data["OfficialHurricWatch"], data["OfficialEvac"], prev, finance, data["Traffic"], data["Evac"])
 
 #res <- gs(X, alpha=0.01)
 #res <- gs(X, alpha=0.05, blacklist = tiers2blacklist(list(c("CloseCoast","CloseWater", sources, "concern"), evac)))
-#res1 <- gs(X, alpha=0.05, blacklist = tiers2blacklist(list(c(demo, personal, house),  c(see, "EvacExpense", official),"Evac")))
-res1 <- gs(X, alpha=0.05, blacklist = tiers2blacklist(list(house, c( see, official, "EvacExpense"),"Evac")))
+#res1 <- gs(X, alpha=0.01, blacklist = tiers2blacklist(list(c(demo, personal, house),  c(see, "EvacExpense", official),"Evac")))
+#res1 <- gs(X, alpha=0.05, blacklist = tiers2blacklist(list(house, c(see, official, local_info, "EvacExpense"), evac)))
+#res1 <- inter.iamb(X, alpha=0.01, blacklist = tiers2blacklist(list(c("CloseCoast", "OfficialHurricWatch", "OfficialEvac", "SrcLocalAuth", "SrcPeers", "SeePeerEvac"), evac)))
+res1 <- iamb(X, alpha=0.01, blacklist = tiers2blacklist(list(c(demo, personal, house),c(prev, official, info, see, concern))))
 #res2 <- hc(X, blacklist = tiers2blacklist(list(reasons, "Evac")))
 #compare(res1, res2)
 #res <- inter.iamb(X)
 #plot(res)
 
+#res1 <- gs(X, alpha=0.01, blacklist = tiers2blacklist(list(c(demo, personal, house),  c(official, see, info, prev, concern),"Evac")))
 graphviz.plot(res1, shape="rectangle")
 
 
--- hurricane/Lili/pcalg_test.R
+++ hurricane/Lili/pcalg_test.R
@@ -1,3 +1,52 @@
-library("pcalg")
-#data("gmG")
-head(gmG)
+library(pcalg)
+
+# pc algorithm, structure learning, based on CI tests
+data(gmG)
+suffStat <- list(C = cor(gmG$x), n = nrow(gmG$x))
+pc.fit <- pc(suffStat, indepTest = gaussCItest, p = ncol(gmG$x), alpha = 0.01)
+require(Rgraphviz)
+par(mfrow = c(1,2))
+plot(gmG$g, main = "True DAG")
+plot(pc.fit, main = "Estimated DAG")
+
+# ges algorithm, structure learning, score-based
+score <- new("GaussL0penObsScore", gmG8$x)
+ges.fit <- ges(score)
+par(mfrow=1:2)
+plot(gmG8$g, main = "")
+plot(ges.fit$essgraph, main = "")
+
+
+# fci algorithm, build on PC algorithm, can include hidden variables
+data(gmL)
+suffStat1 <- list(C = cor(gmL$x), n = nrow(gmL$x))
+pag.est <- fci(suffStat1, indepTest= gaussCItest, p = ncol(gmL$x), alpha = 0.01)
+par(mfrow = c(1,2))
+plot(gmL$g, main = "True DAG")
+plot(pag.est, main = "Estimated PAG")
+
+# rfci algorithm, faster, 
+data(gmL)
+suffStat1 <- list(C = cor(gmL$x), n = nrow(gmL$x))
+pag.est <- rfci(suffStat1, indepTest = gaussCItest, p = ncol(gmL$x), alpha = 0.01, labels = as.character(2:5))
+par(mfrow = c(1,2))
+plot(gmL$g, main = "True DAG")
+plot(pag.est, main = "Estimated PAG")
+
+
+# ida algorithm
+ida(1, 6, cov(gmG$x), pc.fit@graph)
+
+#######################################################################################
+mydata <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels.csv", header=TRUE)
+suffStat <- list(C = cor(mydata), n = nrow(mydata))
+pc.fit <- pc(suffStat, indepTest = gaussCItest, alpha = 0.01, labels = colnames(mydata))
+require(Rgraphviz)
+plot(pc.fit)
+
+mydata <- read.csv("C:/Users/Sophie/workspace/LiliModel/Lili_BN_labels.csv", header=TRUE)
+suffStat <- list(C = cor(mydata), n = nrow(mydata))
+pag.est <- fci(suffStat, indepTest= gaussCItest, alpha = 0.01, labels = colnames(mydata))
+require(Rgraphviz)
+plot(pag.est)
+
