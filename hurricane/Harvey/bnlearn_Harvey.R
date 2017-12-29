0d65945d3a5197a206f2319f49b84c8ff5644390
sophiewxh <huiwang8921@gmail.com>
2017-12-29 11:16:32 -0500

    work on model averaging

--- hurricane/Harvey/bnlearn_Harvey.R
+++ hurricane/Harvey/bnlearn_Harvey.R
@@ -1,35 +1,51 @@
 library(bnlearn)
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("Rgraphviz")
 library(Rgraphviz)
 
-data <- read.csv("C:/Users/Sophie/workspace/Hurricane/MTurk/data/MTurk_Harvey_expand_evac_ability.csv", header=TRUE)
-data <- lapply(data, as.numeric)
+data <- read.csv("hurricane/Harvey/MTurk_Harvey.csv", header=TRUE)
+#data <- lapply(data, as.numeric)
+data <- lapply(data, as.factor)
 X <- as.data.frame(data)
 
-demo <- c("age", "gender", "edu", "income", "househd_size", "r_white", "r_black", "r_asian", "r_hispanic", "r_native", 
+demo <- c("age", "is_male", "edu", "income", "househd_size", "is_white", "is_black", "is_asian", "is_hispanic", "is_native", 
           "has_children", "has_elders", "has_special_needs", "has_pets")
 
-house <- c("hs_single_fam", "hs_condo", "hs_mobile", "hm_wood", "hm_brick", "owner", "insurance", "coast_dist")
+house <- c("house_single_fam", "house_condo", "house_mobile", "house_wood", "house_brick", "is_owner", "has_insurance", "coast_dist")
 
-info_tv <- c("info_tv", "info_tv_damage", "info_tv_casualty", "info_tv_road", "info_tv_prep", "info_tv_stay", "info_tv_leave")
+info_tv <- c("use_tv_radio", "tv_radio_storm_damage", "tv_radio_casualty", "tv_radio_road_traffic", "tv_radio_preparation", "tv_radio_people_stay", "tv_radio_people_leave")
 
-info_social_media <- c("info_social", "info_social_damage", "info_social_storm", "info_social_road", "info_social_prep","info_social_leave", "info_social_stay")
+info_social_media <- c("use_social_media", "social_media_storm_damage", "social_media_storm_strength", "social_media_road_traffic", "social_media_preparation","social_media_people_stay", "social_media_people_leave")
 
-info_other <- c("friends_suggest", "neighbors_doing")
+info_other <- c("friends_suggest_stay", "friends_suggest_evac","neighbors_stay", "neighbors_evac")
 
 evac_notice <- c("received_evac_notice", "no_evac_notice", "received_mandatory", "received_voluntary", 
                  "received_stay_notice", "no_stay_notice", "evac_notice_before_landfall", "evac_notice_after_landfall")
 
-risk <- c("risk_stay", "risk_evac", "wind_safety", "wind_property", "flood_safety", "flood_property")
+risk <- c("risk_of_stay", "risk_of_evac", "wind_risk_to_safety", "wind_risk_to_property", "flood_risk_to_safety", "flood_risk_to_property")
 
 evac_ability <- c("ability_children", "ability_elders", "ability_special_needs", "ability_pets", 
                   "ability_expense", "ability_no_transport", "ability_no_place", "ability_job")
 
 evac_decision <- c("evac_decision")
 
-blklist <- list(c(demo, house, info_tv, info_social_media, info_other, evac_notice), c(risk, evac_ability), evac_decision)
-net <- iamb(X, alpha=0.01, blacklist = tiers2blacklist(blklist))
+#blklist <- list(c(demo, house, info_tv, info_social_media, info_other, evac_notice), c(risk, evac_ability), evac_decision)
+#net <- iamb(X, alpha=0.01, blacklist = tiers2blacklist(blklist))
+#graphviz.plot(net, shape="rectangle")
 
-graphviz.plot(net, shape="rectangle")
+#sapply(X, class)
+#levels(X$age)
+learned <- hc(X)
+graphviz.plot(learned, shape="rectangle")
+score(learned, data=X, type="bic")
+arc.strength(learned, data=X, criterion="bic")
 
-# cannot compute score, because graph is partially directed
-score(net, X, type='bde')
+# model averaging
+boot <- boot.strength(X, R = 1000, algorithm = "hc",
+                      algorithm.args = list(score = "bic"))
+
+boot[(boot$strength > 0.85) & (boot$direction >= 0.5), ]
+avg.boot <- averaged.network(boot, threshold=0.85)
+graphviz.plot(avg.boot, shape="rectangle")
+
